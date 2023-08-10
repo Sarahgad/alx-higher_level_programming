f031d90579d8ed7efb81ed614300aad4e079bfaa
Sarahgad <sarahgad91@gmail.com>
2023-08-10 13:29:37 -0700

    101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    str_2 = ''
+    for i in range(len(str)):
+        if i == n:
+            continue
+        else:
+            str_2 += str[i]
+    return(str_2)
