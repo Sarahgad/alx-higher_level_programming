bc74ac81be905b43ace31addaa2621130eb2cb6f
Sarahgad <sarahgad91@gmail.com>
2023-08-10 12:19:54 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == '__main__':
     from sys import argv
     if len(argv) > 1:
-        print("{0:d} arguments:".format(len(argv) -1))
+        print("{0:d} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{0:d}: {1}".format(i, argv[i]))
     else:
