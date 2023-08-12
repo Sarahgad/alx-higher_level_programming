b0219123ad90d5743d4ba9299ac518892f6ef6c6
Sarahgad <sarahgad91@gmail.com>
2023-08-12 13:41:47 -0700

    3

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
         for element in list:
-            if list.index(element) != len(list) - 1:
-                print("{:d} ".format(element), end="")
-            else:
-                print("{:d}".format(element))
+                print("{:d}".format(element), end="")
+                if list.index(element) != len(list) - 1:
+                    print(" ", end="")
+        print()
