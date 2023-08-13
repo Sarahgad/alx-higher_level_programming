603569adecf37b51a91191cb412da47b289b25ce
Sarahgad <sarahgad91@gmail.com>
2023-08-13 08:09:09 -0700

    7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,10 @@
-#!/usr/bin/python3i
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = ()
-    if len(a) < 2 or len(b) < 2:
-    for i in range (2)
-        new_tuple[i = a[i] + b[i]
+    if len(tuple_a) > 2:
+        tuple_a += (0,) * (2 - len(tuple_a))
+    elif len(tuple_b) < 2:
+        tuple_b += (0,) * (2 - len(tuple_b))
+    for i in range(2):
+        new_tuple += (tuple_a[i] + tuple_b[i],)
+    return (new_tuple)
