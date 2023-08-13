7addac7c472df612fd305897df187c2180624759
Sarahgad <sarahgad91@gmail.com>
2023-08-13 11:42:14 -0700

    11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,6 +4,6 @@ def delete_at(my_list=[], idx=0):
         return (my_list)
     else:
         for i in range(len(my_list)):
-            if i == idx: 
+            if i == idx:
                 del(my_list[i])
         return (my_list)
