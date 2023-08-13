0124db1951907fbbf1928bf3917976d2dd3cb07a
Sarahgad <sarahgad91@gmail.com>
2023-08-13 11:21:09 -0700

    11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx == len(my_list) - 1 or idx < 0:
+        return (my_list)
+    for i in range(len(my_list)):
+        if i == idx:
+            del(my_list[i])
+        else:
+            continue
+    return (my_list)
