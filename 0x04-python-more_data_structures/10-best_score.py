cb979960a26f8d0c3d7b42bc96f085926790b996
Sarahgad <sarahgad91@gmail.com>
2023-08-16 15:52:38 -0700

    10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        dict_sorte = dict(sorted(a_dictionary.items()))
+        for k, v in dict_sorte.items():
+            continue
+        return k
