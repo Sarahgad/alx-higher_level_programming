e2818e73f759b3fcef918b8d79c9deef75d09082
Sarahgad <sarahgad91@gmail.com>
2023-08-29 22:22:59 +0300

    0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
+"""
+python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""
+
+
 class Square:
-    """This is the constructor method for the Square class.
+    """
+    This is the constructor method for the Square class.
     It initializes the square object.
-    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
     """
     pass
