f39865896477f662beab8fb06828d7b7a01aa53b
Sarahgad <sarahgad91@gmail.com>
2023-08-29 14:41:33 -0700

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
-"""Defines a class Square.
-    Attribution: size"""
+"""Defines a class Square."""
 
 
 class Square:
-    """Represents a square. with size."""
+    """Represents a square. with size.
+    Attributes:
+        __size (int): The size of square.
+    """
     def __init__(self, size):
+        """Initializes a Square instance.
+        Args:
+            size (int): the size of square.
+        """
         self.__size = size
