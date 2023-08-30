531299b7b2809991c6fde22a37ff10b8a97085db
Sarahgad <sarahgad91@gmail.com>
2023-08-30 15:37:19 +0300

    6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -73,21 +73,6 @@ class Square:
 
         [print("")for i in range(0, self.__position[1])]
         for j in range(0, self.__size):
-            [print("_", end="") for j in range(0, self.__position[0])]
+            [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
-
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
