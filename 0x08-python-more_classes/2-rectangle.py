7c43367fb815c49ae0a12b9fb2bf8f9ca3d4d850
Sarahgad <sarahgad91@gmail.com>
2023-09-04 22:53:31 +0300

    2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -62,11 +62,3 @@ class Rectangle:
     def perimeter(self):
         """return perimeter of rectangle"""
         return (self.width + self.height) * 2
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
