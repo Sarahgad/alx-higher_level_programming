456c7a74c76923822ff7296295521ec3fc51d17d
Sarahgad <sarahgad91@gmail.com>
2023-09-04 22:27:24 +0300

    1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,16 +11,9 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """intialize the variable of rectangle"""
-        # if not isinstance(width, int):
-        #     raise TypeError("width must be an integer")
-        # elif width < 0:
-        #     raise TypeError("height must be >= 0")
-        # elif not isinstance(height, int):
-        #     raise TypeError("width must be an integer")
-        # elif height < 0:
-        #     raise ValueError("height must be >= 0")
-        self.__width = width
-        self.__height = height
+ 
+        self.width = width
+        self.height = height
 
         @property
         def width(self):
