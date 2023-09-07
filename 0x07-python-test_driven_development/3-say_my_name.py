0cfade6ae174a58e08e1272e5ed73eac5082b674
Sarahgad <sarahgad91@gmail.com>
2023-09-07 16:46:07 +0300

    0

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,7 +6,7 @@ def say_my_name(first_name, last_name=""):
         first_name (str): represent first user name
         last_name  (str): default by empty str represent second user name
     """
-    if not isinstance(first_name, str):
+    if not isinstance(first_name, str) or first_name is None:
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
