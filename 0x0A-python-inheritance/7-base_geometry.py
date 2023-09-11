8173416995d991932c4ecbe92364b879ac644819
Sarahgad <sarahgad91@gmail.com>
2023-09-12 01:55:23 +0300

    1

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,9 +16,3 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-bg = BaseGeometry()
-
-try:
-    bg.integer_validator("name", True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
