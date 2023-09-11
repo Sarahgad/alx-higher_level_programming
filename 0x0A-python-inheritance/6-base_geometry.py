7aa4276ae09b79d4f79dba541826c299f8abbb28
Sarahgad <sarahgad91@gmail.com>
2023-09-11 23:45:33 +0300

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""costruct class sorted list"""
+
+
+class BaseGeometry:
+    """Base Class"""
+
+    def area(self):
+
+        """return Exception raises"""
+        raise Exception("area() is not implemented")
