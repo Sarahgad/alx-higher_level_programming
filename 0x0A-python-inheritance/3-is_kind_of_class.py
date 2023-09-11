35638bf25ed496c4265cc8c174e691a961fd54fa
Sarahgad <sarahgad91@gmail.com>
2023-09-11 23:16:02 +0300

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""constructed booling class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """return True if the object is instance of class
+    or specified class"""
+
+    return isinstance(obj, a_class)
