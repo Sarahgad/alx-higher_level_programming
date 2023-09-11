b3dabe3c62e70a2096e14d1c3f72bc34917b7a00
Sarahgad <sarahgad91@gmail.com>
2023-09-12 01:36:14 +0300

    7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""costruct class sorted list"""
+
+
+class BaseGeometry:
+    """Base Class"""
+
+    def area(self):
+
+        """return Exception raises"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """that validates value:"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
