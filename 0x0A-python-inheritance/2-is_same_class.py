da45326dbbd206fb19b8a01642572c53eba601a4
Sarahgad <sarahgad91@gmail.com>
2023-09-11 23:12:25 +0300

    2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""contructed bool function"""
+"""constructed bool function"""
 
 
 def is_same_class(obj, a_class):
     """check if the obj an instance in the class """
 
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
