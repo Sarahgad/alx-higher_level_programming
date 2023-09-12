a989164e5b3342d626b74da1055e3d072940ccf8
Sarahgad <sarahgad91@gmail.com>
2023-09-12 11:53:59 -0700

    6

--- 0x0B-python-input_output/6-main.py
+++ 0x0B-python-input_output/6-main.py
@@ -1,25 +1,6 @@
-# #!/usr/bin/python3
-# """practicing on input/output python"""
-
-
-# def read_file(filename=""):
-#     """read the file with encoding UTF8"""
-
-#     with open(filename, encoding="UTF8") as myfile:
-#        for line in myfile:
-#             print(line, end="")
 #!/usr/bin/python3
-"""practicing on input/output python"""
-import json
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-def load_from_json_file(filename):
-    """ returns the JSON
-    representation of an
-    object (string)"""
-    with open(filename, 'r') as my_file:
-        data = json.load(my_file)
-        return data
 filename = "my_list.json"
 my_list = load_from_json_file(filename)
 print(my_list)
@@ -45,3 +26,4 @@ try:
     print(type(my_fake))
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
+
