805738434764a5ca97f46b73a5689d7a6e77c9f1
Sarahgad <sarahgad91@gmail.com>
2023-09-12 20:31:00 +0300

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """read the file with encoding UTF8"""
 
     with open(filename, encoding="UTF8") as myfile:
-        print("{}".format(myfile.read()))
+        print(myfile.read())
