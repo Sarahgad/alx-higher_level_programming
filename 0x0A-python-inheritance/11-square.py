6d83f3e20c48a1cd1d77957e0f89ba9349eec28e
Sarahgad <sarahgad91@gmail.com>
2023-09-12 03:59:40 +0300

    11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""multiple inhrient"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """implement all in Rectangle by using size
+    Args:
+    size(int)
+    """
+    def __init__(self, size):
+        """initialize the size as a private valid int"""
+
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """over ride to the area for square"""
+
+        return self.__size ** 2
+
+    def __str__(self):
+        """return the value of base class"""
+
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
