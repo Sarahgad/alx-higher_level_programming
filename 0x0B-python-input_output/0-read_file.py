48a6b47a57fa09f5d134949989196fc259fbac05
Sarahgad <sarahgad91@gmail.com>
2023-09-12 20:17:01 +0300

    0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""practicing on input/output python"""
+
+
+def read_file(filename=""):
+    """read the file with encoding UTF8"""
+
+    with open("filename", encoding="UTF8") as myfile:
+        myfile.read()
