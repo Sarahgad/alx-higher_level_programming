a722a225ee16a6e7e86fe13d6612fef2bc17f333
Sarahgad <sarahgad91@gmail.com>
2023-09-15 00:49:28 +0300

    12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
+"""Technical interview"""
+
+
 def pascal_triangle(n):
+    """ returns a list of lists of integers
+    representing the Pascal’s triangle of n"""
     lists = []*n
-    for i in range (n):
+    for i in range(n):
         lists.append([0]*i)
-        
-    for list in lists: 
-        list.insert(0,1)
+
+    for list in lists:
+        list.insert(0, 1)
         if len(list) > 1:
             list[-1] = 1
     for i in range(2, n):
         for j in range(1, i):
             lists[i][j] = lists[i-1][j-1] + lists[i-1][j]
     return lists
-
-
-    
-    
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-
-if __name__ == "__main__":
-     print_triangle(pascal_triangle(5))
