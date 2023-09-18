a57e807e27df613a3c478b9a8ec1f2807d22288d
Sarahgad <sarahgad91@gmail.com>
2023-09-18 20:10:51 +0300

    5

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,15 +3,18 @@
 the “base” of all other classes in this project."""
 import json
 import csv
+
+
 class Base:
     """ The goal of it is to manage id attribute
     in all your future classes and
     to avoid duplicating the same code
     (by extension, same bugs)"""
     __nb_objects = 0
+
     def __init__(self, id=None):
-        """initialize id 
-        Args: 
+        """initialize id
+        Args:
         id<int> public instance
         """
         self.id = id
@@ -24,27 +27,28 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """return json"""
-        if list_dictionaries == None or list_dictionaries == {}:
+        if list_dictionaries is None or list_dictionaries == {}:
             return "[]"
         else:
             jsonString = json.dumps(list_dictionaries)
             return jsonString
-    
+
     @classmethod
     def save_to_file(cls, list_objs):
         """return list"""
-        file  = cls.__name__ + ".json"
+        file = cls.__name__ + ".json"
         with open(file, "w") as json_file:
-            if list_objs == None:
+            if list_objs is None:
                 json_file.write("[]")
             else:
-                list_json = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+                list_json = cls.to_json_string([obj.to_dictionary()
+                                                for obj in list_objs])
                 json_file.write(list_json)
-    
+
     @staticmethod
     def from_json_string(json_string):
         """return list"""
-        if json_string == None:
+        if json_string is None:
             return "[]"
         else:
             list_dict = json.loads(json_string)
@@ -58,7 +62,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        file  = cls.__name__ + ".json"
+        file = cls.__name__ + ".json"
         try:
             with open(file, "r") as json_newfile:
                 json_data = json_newfile.read()
@@ -67,23 +71,3 @@ class Base:
             return instance
         except FileNotFoundError:
             return []
-
-    @classmethod
-    def save_to_file_csv(cls, list_objs):
-        """return list"""
-        file  = cls.__name__ + ".csv"
-        with open(file, "w", newline="") as csv_file:
-            csvString = csv.writer(csv_file)
-            if list_objs == None:
-                csv_file.writer.writrow("[]")
-            else:
-                csvString.writerow(list_objs)
-
-    @classmethod
-    def load_from_file_csv(cls):
-        """return list"""
-        file  = cls.__name__ + ".csv"
-        with open(file, "r") as csv_file:
-            csvString = csv.reader(csv_file)
-            return csvString
-        
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
@@ -9,6 +8,17 @@ import io
 """test cases for Base module"""
 
 
+class testCodeFormat(unittest.TestCase):
+
+    def test_Squarmodule(self):
+        """Test that we conform to PEP-8."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/base.py',
+                                    'tests/test_models/test_base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+
 class test_Base(unittest.TestCase):
     """this class to test the methods under the Base class"""
 
@@ -17,7 +27,7 @@ class test_Base(unittest.TestCase):
         r1 = Base()
         r2 = Base()
         self.assertEqual(r1.id, r2.id - 1)
-    
+
     def test_threeargs(self):
         """three args"""
         r1 = Base()
@@ -35,12 +45,7 @@ class test_to_json_string(unittest.TestCase):
         jsonstring = r1.to_json_string(list_dictionary)
         self.assertEqual(jsonstring, '[]')
 
-    
-    
-
-
-
 
 if __name__ == '__main__':
     """calling the unit test"""
-    unittest.main() 
+    unittest.main()
