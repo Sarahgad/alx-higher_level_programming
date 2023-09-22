0cb7c44d58583bfbae9d27036db66d461446eac4
Sarahgad <sarahgad91@gmail.com>
2023-09-22 22:14:40 +0300

    update testcases 4

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,8 +11,7 @@ class TestCodeFormat(unittest.TestCase):
     def test_conformance(self):
         """Test that we conform to PEP-8."""
         style = pycodestyle.StyleGuide(quiet=True)
-        result = style.check_files(['models/rectangle.py',
-                                    'tests/test_models/test_rectangle.py'])
+        result = style.check_files(['models/rectangle.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -6,15 +6,14 @@ import io
 from models.square import Square
 
 
-# class testCodeFormat(unittest.TestCase):
+class testCodeFormat(unittest.TestCase):
 
-#     def test_Squarmodule(self):
-#         """Test that we conform to PEP-8."""
-#         style = pycodestyle.StyleGuide(quiet=True)
-#         result = style.check_files(['models/square.py',
-#                                     'tests/test_models/test_square.py'])
-#         self.assertEqual(result.total_errors, 0,
-#                          "Found code style errors (and warnings).")
+    def test_Squarmodule(self):
+        """Test that we conform to PEP-8."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/square.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
 
 class test_Square(unittest.TestCase):
