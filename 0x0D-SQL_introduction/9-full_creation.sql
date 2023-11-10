493359ec26a482104dbcc1bf220bf1510cef35d8
Sarahgad <sarahgad91@gmail.com>
2023-11-10 23:18:01 +0200

    9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,8 +3,10 @@ CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
     score INT);
+
 INSERT INTO second_table (id, name, score)
     VALUES (1, "John", 10),
             (2, "ALEX", 3),
             (3, "BOb", 14),
             (4, "George", 8);
+SELECT * from second_table;
