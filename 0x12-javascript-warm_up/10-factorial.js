ce364937e0602638530a6bb1927e06650b7ef5cd
Sarahgad <sarahgad91@gmail.com>
2023-11-14 00:22:00 +0200

    10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/local/bin/node
+// #!/usr/local/bin/node
+function factorial (n) {
+  if (isNaN(n) || n === 0 || n === 1){
+  return 1;
+  }
+
+
+  return n * factorial(n - 1);
+}
+
+const result = factorial(parseInt(process.argv[2]));
+console.log(result);
