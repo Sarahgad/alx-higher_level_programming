4856a0483af4cb0f03d99fb1645dddc82e8ba018
Sarahgad <sarahgad91@gmail.com>
2023-11-13 22:03:09 +0200

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/local/bin/node
-if(!isNaN(parseInt(process.argv[2], 10))){
-console.log(`My number: ${parseInt(process.argv[2], 10)}`);
-} else{
-    console.log('Not a number');
+if (!isNaN(parseInt(process.argv[2], 10))) {
+  console.log(`My number: ${parseInt(process.argv[2], 10)}`);
+} else {
+  console.log('Not a number');
 }
