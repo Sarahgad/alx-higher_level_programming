e1fe7eda9b07e8ee4e2ca3e35f0a0fecaf3bc8e8
Sarahgad <sarahgad91@gmail.com>
2023-11-15 00:22:18 +0200

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,9 +7,9 @@ class Square extends Square1 {
     if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.size; i++) {
+    for (let i = 0; i < this.height; i++) {
       let row = '';
-      for (let j = 0; j < this.size; j++) {
+      for (let j = 0; j < this.width; j++) {
         row += c;
       }
       console.log(row);
