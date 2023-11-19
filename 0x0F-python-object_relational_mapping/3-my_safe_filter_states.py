407fef8d7aca0012888520c55fcdad056bf90edf
Sarahgad <sarahgad91@gmail.com>
2023-11-19 17:01:10 +0200

    3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Write a script that lists
+all states from the database
+ hbtn_0e_0_usa"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cmd = "SELECT * FROM states WHERE name=%s\
+           ORDER BY states.id ASC", (argv[4],)
+    cur.execute(cmd)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
