40a9453fb0e32af89421d841209eca374020318d
Sarahgad <sarahgad91@gmail.com>
2023-11-22 19:44:23 +0200

    12345

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,67 +1,50 @@
-# # #!/usr/bin/python3
-# # """Start link class to table in database
-# # """
-# # import sys
-# # from model_state import Base, State
-# # from sqlalchemy import (create_engine)
-# # from sqlalchemy.orm import sessionmaker
-
-# # if __name__ == "__main__":
-# #     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-# #                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-# #                            pool_pre_ping=True)
-# #     Base.metadata.create_all(engine)
-# #     Session = sessionmaker(bind=engine)
-# #     session = Session()
-
-# #     list_State = session.query(State).order_by(State.id).all()
-
-# #     for instance in list_State:
-# #         print(f"{instance.id}:  {instance.name}")
-# #     session.close()
 # #!/usr/bin/python3
+# """Start link class to table in database
 # """
-# Lists all State objects from the database hbtn_0e_6_usa
-# """
+# import sys
+# from model_state import Base, State
+# from sqlalchemy import (create_engine)
+# from sqlalchemy.orm import sessionmaker
 
 # if __name__ == "__main__":
-#     from sqlalchemy.orm import sessionmaker
-#     from sqlalchemy import create_engine
-#     from model_state import Base, State
-#     from sys import argv
-
-#     if (len(argv) != 4):
-#         print('Use: username, password database_name')
-#         exit(1)
-
-#     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-#         argv[1], argv[2], argv[3]), pool_pre_ping=True)
+#     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+#                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+#                            pool_pre_ping=True)
 #     Base.metadata.create_all(engine)
-
 #     Session = sessionmaker(bind=engine)
 #     session = Session()
 
-#     states = session.query(State).order_by(State.id)
+#     list_State = session.query(State).order_by(State.id).all()
 
-#     for row in states:
-#         print(f'{row.id}: {row.name}')
+#     for instance in list_State:
+#         print(f"{instance.id}:  {instance.name}")
 #     session.close()
 #!/usr/bin/python3
-"""lists all State objects from the database hbtn_0e_6_usa"""
+"""Start link class to table in the database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+        Base = declarative_base(bind=engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
 
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
+        list_State = session.query(State).order_by(State.id).all()
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+        if not list_State:
+            print("No records found.")
+        else:
+            for instance in list_State:
+                print(f"{instance.id}:  {instance.name}")
 
-    session = Session(engine)
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        session.close()
