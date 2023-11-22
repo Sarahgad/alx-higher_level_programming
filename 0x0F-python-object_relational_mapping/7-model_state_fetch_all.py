16bb7b1dd5272ca1c3673ae4bf64610d7b9697dc
Sarahgad <sarahgad91@gmail.com>
2023-11-22 19:32:29 +0200

    7 repo

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,48 @@
+# #!/usr/bin/python3
+# """Start link class to table in database
+# """
+# import sys
+# from model_state import Base, State
+# from sqlalchemy import (create_engine)
+# from sqlalchemy.orm import sessionmaker
+
+# if __name__ == "__main__":
+#     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+#                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+#                            pool_pre_ping=True)
+#     Base.metadata.create_all(engine)
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
+
+#     list_State = session.query(State).order_by(State.id).all()
+
+#     for instance in list_State:
+#         print(f"{instance.id}:  {instance.name}")
+#     session.close()
 #!/usr/bin/python3
-"""Start link class to table in database
 """
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+Lists all State objects from the database hbtn_0e_6_usa
+"""
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sys import argv
+
+    if (len(argv) != 4):
+        print('Use: username, password database_name')
+        exit(1)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    list_State = session.query(State).order_by(State.id).all()
+    states = session.query(State).order_by(State.id)
 
-    for instance in list_State:
-        print(f"{instance.id}:  {instance.name}")
+    for row in states:
+        print(f'{row.id}: {row.name}')
     session.close()
