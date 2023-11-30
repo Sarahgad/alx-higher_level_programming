7d3eaf8a1decde59d519e799eecfee63cd268de1
Sarahgad <sarahgad91@gmail.com>
2023-11-30 14:12:13 +0200

    tech

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,18 +5,12 @@ def find_peak(list_of_integers):
     """find peak method"""
     if list_of_integers == []:
         return None
-    i = int(len(list_of_integers) / 2)
-    if list_of_integers[i] > list_of_integers [i - 1] and \
-   list_of_integers[i] > list_of_integers [i + 1]:
-        return list_of_integers[i]
-    
-    if list_of_integers[i] >= list_of_integers [i - 1] and list_of_integers[i] < list_of_integers [i + 1]:
-        for i in range(i, len(list_of_integers) - 1, 1):
-            if list_of_integers[i] >= list_of_integers [i - 1] and \
-   list_of_integers[i] >= list_of_integers [i + 1]:
-                return list_of_integers[i]
-    else:
-         for i in range(0, i, 1):
-            if list_of_integers[i] >= list_of_integers [i - 1] and \
-   list_of_integers[i] >= list_of_integers [i + 1]:
-                return list_of_integers[i]
+    low, high = 0, len(list_of_integers) - 1
+    while low < high:
+        mid = (high + low) // 2
+        if list_of_integers[mid] >= list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+    return list_of_integers[low]
+
