4e8d799619b24cb01558052428f4a4a6a377b173
Sarahgad <sarahgad91@gmail.com>
2023-11-30 23:07:19 +0200

    102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request displays the size
-curl -s -w "%{http_code}:%{content_type}" 0.0.0.0:5000/catch_me
+curl -sL -w "%{http_code}:%{content_type}" 0.0.0.0:5000/catch_me
