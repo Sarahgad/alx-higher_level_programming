93663af3e8340e9a00e4eb46f7d02cdee80d89b3
Sarahgad <sarahgad91@gmail.com>
2023-12-01 23:26:48 +0200

    2 Pycodestyle

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,4 +11,3 @@ if __name__ == "__main__":
     request = urllib.request.Request(url=url, data=data, method='post')
     with urllib.request.urlopen(request) as response:
         print(response.read().decode('utf-8'))
-
