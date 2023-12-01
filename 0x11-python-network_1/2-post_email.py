ffd0ca6f92822faeaf82f532110bc2dcabe536fe
Sarahgad <sarahgad91@gmail.com>
2023-12-01 23:18:37 +0200

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     param = {"email": argv[2]}
-    qeurystring = urllib.parse.urlencode(param)
-    url = url + "?" + qeurystring
-    with urllib.request.urlopen(url) as response:
-        print(response.read())
+    qeurystring = urllib.parse.urlencode(param).encode('utf-8')
+    request = urllib.request.Request(url=url, data=param, method='post')
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode('utf-8'))
