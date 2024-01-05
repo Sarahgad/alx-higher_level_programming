7048048f8cd0de39d5cbddb91944c154d9b6dbd4
Sarahgad <sarahgad91@gmail.com>
2024-01-05 12:31:37 +0200

    2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,9 @@
 const request = require('request');
 const urlPath = process.argv[2];
 
-request(urlPath, function (res) {
+request(urlPath, function (err, res) {
+    if (err) {
+        console.log(err)
+    }
   console.log(`code: ${res.statusCode}`);
 });
